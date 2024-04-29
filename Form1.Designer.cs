bfa2099a3892f906ff3cfab6301e4005555ad79e
GtJohnny <ionutxd04@gmail.com>
2024-04-30 01:55:10 +0300

    29.04/30.04 almost done per file

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(43, 51);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1332, 678);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(120, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(794, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1409, 756);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,128 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using System.Windows.Input;
+using Word=Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Word;
+using System.Security.Cryptography.X509Certificates;
+
 
 namespace BARSV_Generator
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private string WriteParagraph(Paragraph p,ref Dictionary<string, string> dict)
+        {
+
+            string str = p.Range.Text;
+            int i = str.Length-1;
+            while (i >0)
+            {
+                i = str.LastIndexOf("}}", i);
+                if (i < 0) break;
+                int j = str.LastIndexOf("{{", i - 2);
+                if (j < 0) break;
+                string key = str.Substring(j+2, i - j - 2);
+                
+                if (dict.ContainsKey(key))
+                {
+                    string v = "{{" + key + "}}";
+                    string n = dict[key];
+
+                    str=str.Replace("{{" + key + "}}", dict[key]);
+                }
+                i = j;
+                
+            }
+            textBox1.Text += str+"\r\n";
+            return str;
+        }
+
+
+
+        public void WriteDocument(ref string path,ref Dictionary<string,string> dict)
+        {
+
+            Word.Application app = new Word.Application();
+            object miss = System.Reflection.Missing.Value;
+            object readOnly = true;
+            Document doc = app.Documents.Open(path,miss,readOnly);
+           
+            int a = 0;
+            for (int i = 0; i < doc.Paragraphs.Count; i++)
+            {
+                try
+                {
+                    doc.Paragraphs[i].Range.Text = WriteParagraph(doc.Paragraphs[i], ref dict);
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            doc.SaveAs2(Environment.CurrentDirectory + "\\Test1.docx");
+            doc.Close();
+
+        }
+
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+            string fis = "#Ordonanta_CFL_Template.docx";
+            string path = Environment.CurrentDirectory+ "\\"+fis;
+
+            
+            Word.Application app = new Word.Application();
+            object miss= System.Reflection.Missing.Value;
+            object readOnly = true;
+
+
+            string ora_accident = "23:29";
+            string criminal = "Marcel Gruiu";
+            Dictionary<string,string> dict = new Dictionary<string,string>();
+            dict["nr_penal"] = "69";
+            dict["anul_acc"] = "2024";
+            dict["luna_acc"] = "4";
+            dict["ziua_acc"] = "29";
+            dict["ora_accident"] = "23:29";
+            dict["locul_accidentului"] = "Craiova";
+            dict["criminalist1"] = "Cezar";
+            dict["criminalist2"] = "Ghergu";
+            dict["nume_agent1"] = "Salam";
+            dict["articol_penal"] = "17";
+            dict["agent1"] = "Mihnea";
+            dict["anul_doc"] = "2024";
+            dict["luna_doc"] = "4";
+            dict["ziua_doc"] = "29";
+            dict["ora_disp"] = "00:49";
+
+            /*  multiple calls */
+
+            WriteDocument(ref path, ref dict);
+
+            
+
+            
+
+
         }
     }
 }
